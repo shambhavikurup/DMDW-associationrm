9e1afa611de4d589380dd65d55cd11d97db823f3
anjali13s <79659570+anjali13s@users.noreply.github.com>
2021-03-27 18:17:39 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -56,20 +56,6 @@ def findFrequentItems(S):
             frequentItems.append(i)
     return frequentItems 
 
-def aprioriMatrix(rowNo, colNo, dataList):
-    mat = []
-    for i in range(rowNo):
-        row = []
-        for j in range(colNo):
-            row.append(dataList[rowNo * i + j])
-        mat.append(rowList)
-
-    return mat
-
-def aprioriTable(support, confidence, lift):
-    table = ['item A and B', support[0], confidence[0], lift[0], 'item A and C', support[1], confidence[1], lift[1], 'item A and D', support[2], confidence[2], lift[2], 'item A and E', support[3], confidence[3], lift[3]]
-    mat = aprioriMatrix(4,4,table)
-    print (mat)
     
 def compare(confidence, lift):
   big_confidence=0
