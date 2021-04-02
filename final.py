9e4042db57922a0dde727fcec5cd29457f09a463
anjali13s <79659570+anjali13s@users.noreply.github.com>
2021-04-03 00:20:15 +0300

    Update final.py

--- final.py
+++ final.py
@@ -1,6 +1,19 @@
 import random
 
-
+def frequence(items_list, trans):
+    items_count = dict() 
+    for i in items_list:  #for every item that is present in the dataset 
+        if(len(items_list[i]=1)): 
+            itemset = {i}
+        else:
+            itemset = set(i)     
+        for j in trans.items():  #for every transaction
+            if itemset.issubset(set(j[1])):   #if the itemset is a subset of that transaction
+                if i in items_count:           
+                    items_count[i] += 1       #the frequency of the itemset increases by 1
+                else:
+                    items_count[i] = 1        #otherwise it remains 1
+    return items_count                        #return the requency of each itemset
 
 
 def main():
